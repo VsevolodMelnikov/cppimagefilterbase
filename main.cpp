017e85f6ac34e99e7548396df79c5927c36676fb
VsevolodMelnikov <55282628+VsevolodMelnikov@users.noreply.github.com>
2019-12-22 12:09:02 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 
 int main( int argc, char *argv[] )
 {
-  char* arg1 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\config.txt";
-  char* arg2 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\in.jpg";
-  char* arg3 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\out.jpg";
-  Manager man(arg1, arg2, arg3);
+  //char* arg1 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\config.txt";
+  //char* arg2 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\in.jpg";
+  //char* arg3 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\out.jpg";
+  Manager man(argv[1], argv[2], argv[3]);
   man.run();
 
   return 0;
