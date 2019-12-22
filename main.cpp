f7dbf9ddb63afffc8ae4a22afe731ef8335adcfa
VsevolodMelnikov <55282628+VsevolodMelnikov@users.noreply.github.com>
2019-12-22 16:31:19 +0300

    god bless clang

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 
 int main( int argc, char *argv[] )
 {
-  //char* arg1 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\config.txt";
-  //char* arg2 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\in.jpg";
-  //char* arg3 = "C:\\Users\\edwar\\source\\repos\\VsevolodMelnikov\\cppimagefilterbase\\files\\out.jpg";
-  if (argc != 4) {
+  if (argc == 4) {
     Manager man(argv[1], argv[2], argv[3]);
     man.run();
   }
