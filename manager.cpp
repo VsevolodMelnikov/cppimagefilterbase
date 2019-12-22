59b2e5eebe49d1e0cb27a41baadcbe20eb098d04
VsevolodMelnikov <55282628+VsevolodMelnikov@users.noreply.github.com>
2019-12-22 14:02:23 +0300

    god help us all

--- manager.cpp
+++ manager.cpp
@@ -12,7 +12,7 @@ Manager::Manager(char* arg1, char* arg2, char* arg3)
   int u, l, d, r;
   string filtname;
 
-  try {
+  /*try {
     while (f >> filtname) {
       f >> u;
       f >> l;
@@ -33,16 +33,42 @@ Manager::Manager(char* arg1, char* arg2, char* arg3)
       }
     }
   }
-  catch (...) {}
+  catch (...) {}*/
+  while (f.eof()) {
+    try {
+      f >> filtname;
+      f >> u;
+      f >> l;
+      f >> d;
+      f >> r;
+
+      if (filtname == (string)"Red") {
+        filters.push_back(new RedFilter(u, l, d, r));
+      }
+      else if (filtname == (string)"Threshold") {
+        filters.push_back(new ThresholdFilter(u, l, d, r));
+      }
+      else if (filtname == (string)"Blur") {
+        filters.push_back(new BlurFilter(u, l, d, r));
+      }
+      else if (filtname == (string)"Edge") {
+        filters.push_back(new EdgeFilter(u, l, d, r));
+      }
+    }
+    catch (...) {
+      break;
+    }
+  }
+  f.close();
 }
 void Manager::run() {
   png_toolkit studTool;
 
   studTool.load(filename_in);
-  /*for (Filter* filt : filters) {
+  for (Filter* filt : filters) {
     image_data data = studTool.getPixelData();
     filt->apply(data);
-  }*/
+  }
   studTool.save(filename_out);
 }
 Manager::~Manager() {
